fac205656a9266e2e3b55c7ce4d8a3536d2c4e0a
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2016-11-15 21:40:28 -0700

    Added scenes and casts joins

---
+++ migrations/20161115185106_students_scenes.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('students_scenes', function(table) {
+    table.increments();
+    table.integer('student_id').references('id').inTable('students').onDelete('CASCADE');
+    table.integer('scene_id').references('id').inTable('scenes').onDelete('CASCADE');
+    table.integer('cast_id').references('id').inTable('casts').onDelete('CASCADE');
+    table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+    table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
+    table.unique(['student_id', 'scene_id', 'cast_id']);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('students_casts');
+};
