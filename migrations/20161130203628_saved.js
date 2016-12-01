5446e24649817789eaf31bc5ee6289fd165dc6d9
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2016-11-30 21:34:12 -0700

    Added saved items table

---
+++ migrations/20161130203628_saved.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('saved', function(table) {
+    table.increments();
+    table.string('userId');
+    table.specificType('savedList', "text[]");
+    table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+    table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('students');
+};
---
+++ seeds/saved.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('saved').del()
+    .then(function () {
+      return Promise.all([
+        // Inserts seed entries
+        knex('saved').insert({id: 1, userId: '1111', savedList: ["a","b","c"]}),
+        knex('saved').insert({id: 2, userId: '2222', savedList: ["A","B","C"]}),
+        knex('saved').insert({id: 3, userId: '3333', savedList: ["aa","bb","cc"]})
+      ]);
+    });
+};
