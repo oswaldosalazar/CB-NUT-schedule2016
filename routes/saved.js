d90fd27e50da21c9fddf47a24f2ac353a0407b04
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2016-12-02 10:05:54 -0700

    Added error catching to http req's

--- routes/saved.js
+++ routes/saved.js
@@ -1,28 +1,59 @@
 var express = require('express');
 var router = express.Router();
-// var request = require('request');
 var saved = require('../database/saved');
+
+const oops = "Something went wrong on our end.  Please try again."
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  if (!req.query.id) {
+    var err = new Error('Bad Request');
+    err.status = 400;
+    next(err);
+    return;
+  }
   saved.getSaved(req.query.id)
   .then( (data) => {
     res.send(data)
   })
+  .catch( (err) => {
+    console.log(err);
+    res.send(oops);
+  })
 });
 
 router.post('/', function(req, res, next) {
+  if (!(req.body.userId && req.body.savedList)) {
+    var err = new Error('Bad Request');
+    err.status = 400;
+    next(err);
+    return;
+  }
   saved.postSaved(req.body)
   .then( (data) => {
     res.send('New data saved')
   })
-})
+  .catch( (err) => {
+    console.log(err);
+    res.send(oops);
+  })
+});
 
 router.put('/', function(req, res, next) {
+  if (!(req.body.userId && req.body.savedList)) {
+    var err = new Error('Bad Request');
+    err.status = 400;
+    next(err);
+    return;
+  }
   saved.updateSaved(req.body)
   .then( (data) => {
-    res.send('Data updated saved')
+    res.send('Data updated')
   })
-})
+  .catch( (err) => {
+    console.log(err);
+    res.send(oops);
+  })
+});
 
 module.exports = router;
