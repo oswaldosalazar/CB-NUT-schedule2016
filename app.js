738ebc51e5ac42e710e6164d5f5eb05dfc53b0f0
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2016-12-02 23:15:33 -0700

    added cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -29,6 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
