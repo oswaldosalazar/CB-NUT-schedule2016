09ad3108c60196dca1d2e5e8534cfa21b7ada648
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2016-12-04 11:19:31 -0700

    edit scenes seed

--- seeds/scenes.js
+++ seeds/scenes.js
@@ -5,18 +5,18 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('scenes').insert({id: 1, scene: 'partyBoys'}),
-        knex('scenes').insert({id: 2, scene: 'partyGirls'}),
-        knex('scenes').insert({id: 3, scene: 'cousins'}),
-        knex('scenes').insert({id: 4, scene: 'hornBoys'}),
-        knex('scenes').insert({id: 5, scene: 'soldiers'}),
-        knex('scenes').insert({id: 6, scene: 'cavalry'}),
-        knex('scenes').insert({id: 7, scene: 'sugarPlumAttendants'}),
-        knex('scenes').insert({id: 8, scene: 'angelsRight'}),
-        knex('scenes').insert({id: 9, scene: 'angelsLeft'}),
-        knex('scenes').insert({id: 10, scene: 'polichinellesClowns'}),
-        knex('scenes').insert({id: 11, scene: 'polichinellesDolls'}),
-        knex('scenes').insert({id: 12, scene: 'motherGingerLegs'}),
+        knex('scenes').insert({id: 1, scene: 'Party Boys'}),
+        knex('scenes').insert({id: 2, scene: 'Party Girls'}),
+        knex('scenes').insert({id: 3, scene: 'Cousins'}),
+        knex('scenes').insert({id: 4, scene: 'Horn Boys'}),
+        knex('scenes').insert({id: 5, scene: 'Soldiers'}),
+        knex('scenes').insert({id: 6, scene: 'Cavalry'}),
+        knex('scenes').insert({id: 7, scene: 'Sugar Plum Attendants'}),
+        knex('scenes').insert({id: 8, scene: 'Angels Right'}),
+        knex('scenes').insert({id: 9, scene: 'Angels Left'}),
+        knex('scenes').insert({id: 10, scene: 'Polichinelles Clowns'}),
+        knex('scenes').insert({id: 11, scene: 'Polichinelles Dolls'}),
+        knex('scenes').insert({id: 12, scene: 'Mother Ginger Legs'}),
       ]);
     });
 };
